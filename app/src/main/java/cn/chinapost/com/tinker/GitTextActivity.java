327ca4e5dba7e7b93c1398c917df0fc8c10bf8bf
tomcatzdy <tomcat_zdy@163.com>
2019-03-29 14:44:34 +0800

    添加测试git

---
+++ app/src/main/java/cn/chinapost/com/tinker/GitTextActivity.java
@@ -0,0 +1,13 @@
+package cn.chinapost.com.tinker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_text);
+    }
+}
